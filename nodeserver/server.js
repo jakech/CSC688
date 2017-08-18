e571b715d3aa260484060145f322f3e93915ffb5
Jake <jake83@gmail.com>
2017-08-18 15:47:08 -0700

    config file for nodeserver

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var PROTO_PATH = path.resolve(__dirname, '../proto/rpc.proto')
+var config = require('./config')
 
 var grpc = require('grpc')
 var rpc_proto = grpc.load(PROTO_PATH).rpc
@@ -9,7 +10,10 @@ var category = require('./category')
 function main() {
     var server = new grpc.Server()
     server.addService(rpc_proto.Rpc.service, category)
-    server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure())
+    server.bind(
+        config.host + ':' + config.port,
+        grpc.ServerCredentials.createInsecure()
+    )
     server.start()
 }
 
